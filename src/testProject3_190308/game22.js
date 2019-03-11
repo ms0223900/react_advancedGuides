3fcee4e7007454d5c265f3d33d532bdac3c6e921
ms0223900 <ms0223900@gmail.com>
2019-03-11 17:50:25 +0800

    190311_update dots

--- src/testProject3_190308/game22.js
+++ src/testProject3_190308/game22.js
@@ -1,17 +1,26 @@
 /* eslint-disable no-unused-vars */
 
 const $id = (id) => document.getElementById(id)
-$id('root').width = '100'
-$id('root').height = '100'
+const canvas = {
+  w: 120,
+  h: 140,
+}
+canvas.wBlocks = canvas.w / 20
+canvas.hBlocks = canvas.h / 20
+
+
+console.log(canvas.wBlocks)
+$id('root').width = canvas.w
+$id('root').height = canvas.h
 
 const ctx = $id("root").getContext("2d")
 
 const drawBrick = (mapN, color='#ddd') => {
   ctx.fillStyle = color
-  ctx.fillRect(mapN % (100 / 20) * 20 + 1, ~~(mapN / (100 / 20)) * 20 + 1, 18, 18)
+  ctx.fillRect(mapN % (canvas.wBlocks) * 20 + 1, ~~(mapN / (canvas.wBlocks)) * 20 + 1, 18, 18)
 }
 const convertToXY = (p) => {
-  return [p % (100 / 20) * 20, ~~(p / (100 / 20)) * 20]
+  return [p % (canvas.wBlocks) * 20, ~~(p / (canvas.wBlocks)) * 20]
 }
 const drawPac = (p, color='#ff0') => {
   ctx.fillStyle = color
@@ -43,13 +52,13 @@ const checkCollide = (t, item) => {
 const checkAllCollide = (t, items) => {
   for (let i = 0; i < items.length; i++) {
     if(checkCollide(t, items[i])) {
-      console.log(items[i])
+      // console.log(items[i])
       return true
     }
   }
   return false
 }
-console.log(checkCollide([59, 20], 9))
+// console.log(checkCollide([59, 20], 9))
 
 // get next position
 const getNextPos = (dir, posNow) => {
@@ -66,53 +75,60 @@ const getNextPos = (dir, posNow) => {
       return [posNow[0] + 2, posNow[1]]
   }
 }
-const getPrevPos = (dir, posNow) => {
-  switch (dir) {
-    case 'up':
-      return [posNow[0], posNow[1] + 2]
-    case 'down':
-      return [posNow[0], posNow[1] - 2]
-    case 'left':
-      return [posNow[0] + 2, posNow[1]]
-    case 'right':
-      return [posNow[0] - 2, posNow[1]]
-    default:
-      return [posNow[0] - 2, posNow[1]]
+
+
+
+// const MAP = [0, 1, 2, 3, 4, 5, 9, 12, 15, 19, 20, 21, 22, 23, 24]
+const MAP = [0, 1, 2, 3, 4, 5, 6, 11, 14, 16, 17, 20, 23, 24, 26, 27, 28, 30, 36, 37, 38, 39, 40, 41]
+const generateDots = () => {
+  let arr = []
+  const dotsAmount = canvas.wBlocks * canvas.hBlocks
+  for (let i = 0; i < dotsAmount; i++) {
+    if(MAP.indexOf(i) === -1) {
+      arr = [...arr, i]
+    }
   }
+  console.log(arr)
+  return arr
 }
+let DOTS = generateDots()
 
 
-
-const MAP = [0, 1, 2, 3, 4, 5, 9, 12, 15, 19, 20, 21, 22, 23, 24]
 let posInit = false
 let pacPosNow = [20, 20]
 let direction = 'right'
 
 
-function drawMap() {
-  for (let i = 0; i < MAP.length; i++) {
-    drawBrick(MAP[i])
+function drawAll(blocks, color) {
+  for (let i = 0; i < blocks.length; i++) {
+    drawBrick(blocks[i], color)
   }
 }
-drawMap()
 
-function game() {
-  let timer = setInterval(() => {
-    // if(posInit) { drawPac(getPrevPos(direction, pacPosNow), '#fff') }
-    ctx.clearRect(0, 0, 100, 100)
-    drawMap()
-    drawPac(pacPosNow, '#ff0')
-    
-    console.log(checkAllCollide(pacPosNow, MAP))
-    pacPosNow = !checkAllCollide(getNextPos(direction, pacPosNow), MAP) ? getNextPos(direction, pacPosNow) : pacPosNow
-    console.log(pacPosNow)
-    // pacPosNow = checkCollide(pacPosNow + 1, MAP) ? pacPosNow : pacPosNow + 1
-    posInit = true
-  }, 50)
+drawAll(MAP)
+let timer
+function gameStart() {
+  timer = setInterval(game, 50)
 }
-game()
+function game() {
+   // if(posInit) { drawPac(getPrevPos(direction, pacPosNow), '#fff') }
+   ctx.clearRect(0, 0, canvas.w, canvas.h)
+   drawAll(MAP)
+   drawAll(DOTS, '#aaa')
+   drawPac(pacPosNow, '#ff0')
+   
+  //  console.log(checkAllCollide(pacPosNow, MAP))
+  pacPosNow = !checkAllCollide(getNextPos(direction, pacPosNow), MAP) ? getNextPos(direction, pacPosNow) : pacPosNow
+  DOTS = checkAllCollide(pacPosNow, DOTS) ? DOTS.filter(dot => !checkCollide(pacPosNow, dot)) : DOTS
+  
+  //  console.log(pacPosNow)
+   // pacPosNow = checkCollide(pacPosNow + 1, MAP) ? pacPosNow : pacPosNow + 1
+   posInit = true
+}
+gameStart()
 
 
-document.addEventListener('keyup', function(e) {
-  direction = ['left', 'up', 'right', 'down'][e.keyCode - 37]
+document.addEventListener('keydown', function(e) {
+  direction = !checkAllCollide(getNextPos(['left', 'up', 'right', 'down'][e.keyCode - 37], pacPosNow), MAP) ? ['left', 'up', 'right', 'down'][e.keyCode - 37] : direction
+  game()
 })
