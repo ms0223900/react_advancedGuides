a52f9d28f6d94c4dabc252b5a30a38ccf6cc3ef7
ms0223900 <ms0223900@gmail.com>
2019-03-12 17:44:21 +0800

    190312_ class pacMan

--- src/testProject3_190308/game22.js
+++ src/testProject3_190308/game22.js
@@ -77,28 +77,29 @@ const getNextPos = (dir, posNow) => {
 }
 const getRandom = (arr) => arr[~~(Math.random() * arr.length)]
 
-//enemy
+//pacMan
 class enemy {
-  constructor(enemyDir='right', enemyPos=[20, 100]) {
+  constructor(enemyDir='right', enemyPos=[20, 100], color='#fa0') {
     this.enemyDir = enemyDir
     this.enemyPos = enemyPos
+    this.color = color
   }
   move() {
     //enemy move to the aisle
-    const dirPosOfArr = directions.indexOf(this.enemyDir)
-    const notNowDir_AndNotCollide = directions.filter(d => checkAllCollide(getNextPos(d, this.enemyPos), MAP) === false).filter(d => d !== reverseDir[dirPosOfArr])
+    const dirPosOfArr = DIRECTIONS.indexOf(this.enemyDir)
+    const notNowDir_AndNotCollide = 
+      DIRECTIONS
+        .filter(d => checkAllCollide(getNextPos(d, this.enemyPos), MAP) === false)
+        .filter(d => d !== reverseDir[dirPosOfArr])
     if(checkAllCollide(this.enemyPos, [Enemy1.enemyPos, Enemy2.enemyPos].filter(pos => pos !== this.enemyPos))) {
       this.enemyDir = reverseDir[dirPosOfArr]
     } else {
       this.enemyDir = notNowDir_AndNotCollide.length > 1 ? getRandom(notNowDir_AndNotCollide) : this.enemyDir
     }
-
-    
     // console.log(notNowDir_AndNotCollide)
 
     // collid with other enemy
-    
-
+  
     //enemies move after colliding wall 
     if(checkAllCollide(getNextPos(this.enemyDir, this.enemyPos), MAP)) {
       const notMeReverseDir = [...reverseDir.slice(0, dirPosOfArr), ...reverseDir.slice(dirPosOfArr + 1)]
@@ -108,15 +109,51 @@ class enemy {
     }
     // console.log(this.enemyDir)
     this.enemyPos = !checkAllCollide(getNextPos(this.enemyDir, this.enemyPos), MAP) ? getNextPos(this.enemyDir, this.enemyPos) : this.enemyPos
+
+    drawPac(this.enemyPos, this.color)
+  }
+  
+}
+//enemy
+class pacMan {
+  constructor(direction='right', pacPosNow=[20, 100], color='#ff0') {
+    this.direction = direction
+    this.pacPosNow = pacPosNow
+    this.color = color
+    this.bufferCount = 1
+    this.bufferDir = ''  
+  }
+  move() {
+    const pacPosNext = getNextPos(this.direction, this.pacPosNow)
+    if(this.bufferDir.length > 1) {
+      const pacPosNextNext = getNextPos(this.bufferDir, pacPosNext)
+      if(!checkAllCollide(pacPosNextNext, MAP)) {
+        this.bufferCount += 1
+      }
+    }
+    if(this.bufferCount % 2 === 0) {
+      this.direction = this.bufferDir
+      this.bufferDir = ''
+      this.bufferCount = 1
+    }
+    console.log(this.direction)
+    this.pacPosNow = !checkAllCollide(pacPosNext, MAP) ? pacPosNext : this.pacPosNow
+   
+    //eat enemy
+    enemiesArr = enemiesArr.filter(e => !checkCollide(this.pacPosNow, e.enemyPos))
+    //eat dots
+    DOTS = checkAllCollide(this.pacPosNow, DOTS) ? DOTS.filter(dot => !checkCollide(this.pacPosNow, dot)) : DOTS
+    drawPac(this.pacPosNow, this.color)
   }
 }
 // const MAP = [0, 1, 2, 3, 4, 5, 9, 12, 15, 19, 20, 21, 22, 23, 24]
 const MAP = [0, 1, 2, 3, 4, 5, 6, 11, 12, 14, 16, 17, 18, 23, 24, 26, 27, 28, 30, 35, 36, 37, 38, 39, 40, 41]
-const directions = ['left', 'up', 'right', 'down']
+const DIRECTIONS = ['left', 'up', 'right', 'down']
 const reverseDir = ['right', 'down', 'left', 'up']
-const Enemy1 = new enemy('right', [20, 100])
-const Enemy2 = new enemy('right', [20, 20])
-const enemiesPosArr = [Enemy1.enemyPos, Enemy2.enemyPos]
+const PacMan = new pacMan('left', [80, 20])
+const Enemy1 = new enemy('right', [20, 100], '#f70')
+const Enemy2 = new enemy('right', [80, 60], '#09f')
+let enemiesArr = [Enemy1, Enemy2]
 
 const generateDots = () => {
   let arr = []
@@ -132,48 +169,34 @@ const generateDots = () => {
 let DOTS = generateDots()
 let posInit = false
 let pacPosNow = [20, 20]
-let pacEnemy = [20, 100]
 let direction = 'right'
-let enemyDir = 'right'
 let timer
 
-
-
-
 //--------------------------------------------------------
-
 
 function gameStart() {
   timer = setInterval(game, 50)
 }
 function game() {
-   // if(posInit) { drawPac(getPrevPos(direction, pacPosNow), '#fff') }
-   ctx.clearRect(0, 0, canvas.w, canvas.h)
-   drawAll(MAP)
-   drawAll(DOTS, '#aaa')
-   drawPac(pacPosNow, '#ff0')
-   drawPac(Enemy1.enemyPos, '#f70')
-   drawPac(Enemy2.enemyPos, '#09f')
-   
-  //  console.log(checkAllCollide(pacPosNow, MAP))
-  pacPosNow = !checkAllCollide(getNextPos(direction, pacPosNow), MAP) ? getNextPos(direction, pacPosNow) : pacPosNow
-  DOTS = checkAllCollide(pacPosNow, DOTS) ? DOTS.filter(dot => !checkCollide(pacPosNow, dot)) : DOTS
-
+  // if(posInit) { drawPac(getPrevPos(direction, pacPosNow), '#fff') }
+  ctx.clearRect(0, 0, canvas.w, canvas.h)
+  drawAll(MAP)
+  drawAll(DOTS, '#aaa')
+  
+  //pacman
+  PacMan.move()
   //enemy
-  Enemy2.move()
-  Enemy1.move()
-
-  //  console.log(pacPosNow)
-   // pacPosNow = checkCollide(pacPosNow + 1, MAP) ? pacPosNow : pacPosNow + 1
-   posInit = true
+  for (let i = 0; i < enemiesArr.length; i++) {
+    enemiesArr[i].move()
+  }
+  posInit = true  
 }
 gameStart()
 
-
-
-
 document.addEventListener('keydown', function(e) {
-  direction = !checkAllCollide(getNextPos(directions[e.keyCode - 37], pacPosNow), MAP) ? directions[e.keyCode - 37] : direction
-  game()
+  const { direction, pacPosNow } = PacMan
+  PacMan.direction = !checkAllCollide(getNextPos(DIRECTIONS[e.keyCode - 37], pacPosNow), MAP) ? DIRECTIONS[e.keyCode - 37] : direction
+  PacMan.bufferDir = DIRECTIONS[e.keyCode - 37]
+  // game()
 })
 $id('start').onclick = gameStart
